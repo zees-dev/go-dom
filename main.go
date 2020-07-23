c7bc5aa8a61ea78ac96d11ac4aeed82726be55a2
zees-dev <dev.786zshan@gmail.com>
2020-07-23 17:03:19 +1200

    updated main go file to call all variations of GetElementByID functions

--- main.go
+++ main.go
@@ -9,12 +9,24 @@ import (
 
 func main() {
 	dom := models.GetExampleDom()
+
 	element, err := dom.GetElementByID("copyright")
 	if err != nil {
 		fmt.Errorf("%s", err)
 	}
-	// element := dom.GetElementByIDBFS("copyright")
-
 	fmt.Println(element)
+
+	element, err = dom.GetElementByIDBFS("copyright")
+	if err != nil {
+		fmt.Errorf("%s", err)
+	}
+	fmt.Println(element)
+
+	element, err = dom.GetElementByIDDFS("copyright")
+	if err != nil {
+		fmt.Errorf("%s", err)
+	}
+	fmt.Println(element)
+
 	time.Sleep(200 * time.Millisecond)
 }
